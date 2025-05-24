a4af65307ca6885f03f4c668af8fed4c355addb5
taodev-web <taofeeq476@gmail.com>
2025-05-24 05:56:33 +0100

    allowed host update

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't ru n with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS',).split("")
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
 
 
 # Application definition
