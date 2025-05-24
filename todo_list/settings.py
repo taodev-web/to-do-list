463b324a71f4495c5a9cfe12660a50c100d5553c
taodev-web <taofeeq476@gmail.com>
2025-05-24 05:29:11 +0100

    Updated ALLOWED_HOSTS and fixed deploy error

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%k$q&g-j5(jvtr757^n2edxuk+aeh+e(v*&bh9pt7d@9oj!7oh'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECURITY WARNING: don't ru n with debug turned on in production!
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS',).split("")
 
 
 # Application definition
@@ -75,10 +74,15 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+database_url = os.environ.get('DATABASE_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
